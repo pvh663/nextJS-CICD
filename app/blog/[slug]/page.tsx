39c0e4bc382644ebdab09dc7d89364bd184853b9
pvh663 <pvhdz2k3@gmail.com>
2024-06-01 15:29:23 +0700

    init project

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,17 @@
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` };
+}
+
+export default function Page({ params }: Params) {
+  return <>
+  <h1>Slug: {params.slug}</h1>
+  <p>PhamHa</p>
+  <p>Wellcome to Ha Noi</p>
+  </>;
+}
